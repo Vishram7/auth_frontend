e5ef7432e81b41552e270a59666596d0b37d2cbe
vishram <rvishram17@gmail.com>
2023-10-25 13:00:12 +0530

    update frontend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Home from './pages/home';
+import Home from './pages/Home';
 import {Routes, Route} from "react-router-dom";
 
 function App() {
